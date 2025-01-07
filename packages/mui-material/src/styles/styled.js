625ff23ed3012148c24590dfc4cee58b07e579d5
Illia Lukianov <lykianovib@gmail.com>
2025-01-07 15:54:51 +0700

    Update styled.js according to proposal
    https://github.com/mui/material-ui/issues/43477#issuecomment-2486577542
    
    Signed-off-by: Illia Lukianov <lykianovib@gmail.com>

--- packages/mui-material/src/styles/styled.js
+++ packages/mui-material/src/styles/styled.js
@@ -4,9 +4,6 @@ import defaultTheme from './defaultTheme';
 import THEME_ID from './identifier';
 import rootShouldForwardProp from './rootShouldForwardProp';
 
-export { default as slotShouldForwardProp } from './slotShouldForwardProp';
-export { default as rootShouldForwardProp } from './rootShouldForwardProp';
-
 const styled = createStyled({
   themeId: THEME_ID,
   defaultTheme,
